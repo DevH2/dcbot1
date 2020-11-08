afb8611e7f475affdaccb923b0fe3444fbaeb6d2
DevH2 <56325948+DevH2@users.noreply.github.com>
2020-11-07 21:15:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,8 @@ const PREFIX = '>';
 const CURRENT_VERSION = '1.0.0';
 const NAME = 'GoodSuggestions';
 const PFP_LINK = 'https://codehs.com/uploads/30b2789baa3f62c94da34ec5bf1c4f1b';
-var queue = new Map();
+var queueMap = new Map();
 
-//const ffmpeg = require("ffmpeg");
 const youtubeDownloader = require('ytdl-core');
 botClient.login(TOKEN);
 
@@ -26,8 +25,8 @@ botClient.on('message', msg =>{
     case 'ping':
       msg.channel.send("You got played.");
       break;
-    case 'syk':
-      msg.channel.send("P.");
+    case 'kys':
+      msg.channel.send("Put a hairdryer in a bathtub.");
       break;
     case 'info':
       if(args[1] === "-v"){
@@ -41,7 +40,7 @@ botClient.on('message', msg =>{
         .setAuthor(NAME + " Help", PFP_LINK )
         .setColor('BLACK')
         .addFields(
-          {name: "Commands", value: " ----------" }
+          {name: "Commands", value: "Devin make an html file later and put in dropbox to get a public link \n >help;>info -v;-ping;-kys;play/p;stop. \n Figure out what they do on your own. " }
         );
       msg.channel.send(helpEmbed);
       break;
@@ -78,37 +77,95 @@ botClient.on('message', msg =>{
 });
 
 //Youtube
-botClient.on('message', async message => {
+botClient.on('message', async message =>{
   if(!message.content.startsWith(PREFIX) || message.author.bot) return;
   let args = message.content.substring(PREFIX.length).split(" ");
+  let songQueue = queueMap.get(message.guild.id);
 
-  if(message.content.startsWith(`${PREFIX}play`) || message.content.startsWith(`${PREFIX}p`)){
-    if(!args[1].includes("https")) return message.channel.send("Please enter a proper URL");
+  if(args[1] == 'play'){
     const voiceChannel = message.member.voice.channel;
-    if(!voiceChannel) return message.channel.send(`You are not in a voice channel ${message.author}.`);
+    if(!voiceChannel) return message.channel.send("You are not in a voice channel.");
+
     const perms = voiceChannel.permissionsFor(message.client.user);
+    if(!perms.has('CONNECT')) return message.channel.send("I do not have permission to connect to the voice channel.");
+    if(!perms.has('SPEAK')) return message.channel.send("I do not have permission to speak in this channel.");
 
-    if(!perms.has('CONNECT')) return message.channel.send("I do not have permission to connect to this voice channel.");
-    if(!perms.has('SPEAK')) return message.channel.send("I do not have permission to speak in this voice channel.");
-
-    try {
-      var connection = await voiceChannel.join();
-    } catch(error) {
-      console.log("There was an error in connecting to the voice channel.");
-      return message.channel.send('There was an error in connecting to the voice channel.');
+    const songInfo = await youtubeDownloader.getInfo(args[1]);
+    const song = {
+      title: songInfo.title,
+      song_url: songInfo.video_url,
     }
 
-    const dispatcher = connection.play(youtubeDownloader(args[1], {quality: 'highestaudio', highWaterMark: 1 << 25}))
-    .on('finish', () => {
-      voiceChannel.leave();
+    if(!songQueue){
+      const queueCreate = {
+        voiceChannel: voiceChannel,
+        connection = null,
+        volume: 5,
+        songs = [],
+      }
+      queueMap.set(message.guild.id, queueCreate);
+      queueCreate.songs.push(songs);
+      try {
+        var connection = await voiceChannel.join();
+        queueCreate.connection = connection;
+        play(message.guild.id, queueCreate.songs[0]);
+      } catch (error) {
+        console.log(error);
+        queueMap.delete(message.guild.id);
+        return message.channel.send("There was an error connecting to the voice channel.");
+      }
+
+    } else {
+      songQueue.songs.push(song);
+      return message.channel.send(`${song.title} has been added to the queue.` )
+    }
+    return;
+
+  } else if(args[1] == 'stop') {
+    const voiceChannel = message.member.voice.channel;
+    if(!voiceChannel) return message.channel.send("You are not in a voice channel.");
+    if(!songQueue) return message.channel.send("There is nothing playing rn");
+    songQueue.songs = [];
+    songQueue.connection.dispatcher.end();
+    message.channel.send("The music has been stopped.")
+    return;
+  } else if(args[1] == 'skip'){
+    if(!voiceChannel) return message.channel.send("You are not in a voice channel.");
+    if(!songQueue) return message.channel.send("There is nothing playing rn");
+    songQueue.connection.dispatcher.end();
+    message.channel.send("The song has been skipped.");
+    return;
+  }
+});
+
+function play(g,song){
+  const songQueue = queueMap.get(g);
+  if(!song){
+    songQueue.voiceChannel.leave();
+    queueMap.delete(g);
+    return;
+  }
+  const dispatcher = connection.play(youtubeDownloader(song.song_url, {quality: 'highestaudio', highWaterMark: 1 << 25}))
+    .on('finish', () =>{
+      songQueue.songs.shift();
+      play(g,songQueue.songs[0]);
     })
     .on('error', error => {
       console.log(error);
+      message.channel.send("There was an error playing the requested song.");
     })
+    dispatcher.setVolumeLogarithmic(songQueue.volume / 5);
+}
 
-    dispatcher.setVolumeLogarithmic(5 / 5);
-  } else if(message.content.startsWith(`${PREFIX}stop`)) {
-    if(!message.member.voice.channel) return message.channel.send(`You are not in a voice channel ${message.author}.`);
-    return message.member.voice.channel.leave();
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+
